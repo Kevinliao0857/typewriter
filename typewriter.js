eca069f5b2f324e19e707a283b7e110d43931812
Kevinliao0857 <kevinliao0857@gmail.com>
2021-02-09 22:23:45 +0000

    It works but cleaner

--- typewriter.js
+++ typewriter.js
@@ -1,12 +1,3 @@
-// const sentence = "hello there from lighthouse labs"
-
-// let output = []
-// let words = sentence
-// for( i = 0; i < words.length; i++){
-//   output += words[i]
-// }
-// console.log(output)
-
 const sentence = "hello there from lighthouse labs\n";
  
 let delayTime = 0
